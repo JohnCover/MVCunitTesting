43f9e44afbb211e53db2f0996e9a84778e96a269
JohnCover <JohnCover@gmail.com>
2017-10-15 21:45:38 -0700

    git migration

---
+++ MyCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyCalculator.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        [TestCategory("Add")]        [Ignore]
+        public void AddSimple()
+        {
+            //arrage
+            Calculator calculator = new Calculator();
+            //act
+            int sum = calculator.Add(1, 2);
+            //assert
+            Assert.AreEqual(3, sum);
+        }
+
+        [TestMethod]
+        public void DivideSimple()
+        {
+            Calculator calculator = new Calculator();
+            int quotient = calculator.Divide(10, 5);
+            Assert.AreEqual(2, quotient);
+        }        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivideByZero()
+        {
+            Calculator calculator = new Calculator();
+            calculator.Divide(10, 0);
+        }
+    }
+}
